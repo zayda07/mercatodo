431589ca0504395dc24df47f4afa5493053cf5d5
Carolina Samaca <carolinasamacagonzalez@gmail.com>
2021-10-12 10:52:40 -0500

    administrador

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import Login from "./components/Login/Login";
+import { Login } from "./components/Login/Login";
 
 function App() {
     return (
-        <div className="App">
+        <div className="background">
             <Login />
         </div>
     );
