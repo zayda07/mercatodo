0fbb3d842ae86d2672aa36a455d88b93c44fde84
Zayda Rivera <zayda07@misena.edu.co>
2021-10-13 10:37:57 -0500

    se hace migracion cambio fecha en models

--- django/mercatodo_backend/api/models.py
+++ django/mercatodo_backend/api/models.py
@@ -5,6 +5,6 @@ class Products(models.Model):
   pro_name = models.CharField(max_length=50)
   pro_provider = models.CharField(max_length=50)
   pro_existences = models.PositiveBigIntegerField()
-  pro_date = models.DateField()
+  pro_date = models.DateTimeField()
   pro_description = models.CharField(max_length=150)
   pro_category = models.CharField(max_length=50)
