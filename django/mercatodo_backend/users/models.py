51cc52dc774ec10ab139cea304943e07ba02d32d
Zayda Rivera <zayda07@misena.edu.co>
2021-10-13 17:41:31 -0500

    se agrega carpeta users

--- django/mercatodo_backend/users/models.py
+++ django/mercatodo_backend/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import manager
 
 # Create your models here.
 class User(AbstractUser):
@@ -11,4 +12,4 @@ class User(AbstractUser):
 
   USERNAME_FIELD = 'email'
   REQUIRED_FIELDS = []
-  
+
