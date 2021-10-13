a65c21af82343e2542ed0ee29450ddf42d5c5b33
Zayda Rivera <zayda07@misena.edu.co>
2021-10-13 16:02:28 -0500

    se crea carpeta users

--- django/mercatodo_backend/mercatodo_backend/settings.py
+++ django/mercatodo_backend/mercatodo_backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'api'
+    'api',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
