e6d906aa21a5c36007f8ce063c42392a670d395c
Carolina Samaca <carolinasamacagonzalez@gmail.com>
2021-10-13 09:57:29 -0500

    login

--- front-end/src/components/Login/Login.jsx
+++ front-end/src/components/Login/Login.jsx
@@ -5,34 +5,44 @@ import { BiUser } from "react-icons/bi";
 import { BiLockAlt } from "react-icons/bi";
 
 export const Login = () => {
-  return (
-    <div className={styles.prueba}>
-      <div className={styles.section}>
-        <img src={logo} alt="MercaTodo" />
-        <p className={styles.merca}>
-          Merca<span className={styles.todo}>Todo</span>
-        </p>
-        <p className={styles.leyenda}>Bienvenido a su mercado virtual</p>
-      </div>
-      <div>
-        <form className={styles.containerLogin}>
-          <div className={styles.input}>
-            <input className={styles.inputUser} type="text" required />
-            <label className={styles.labelUser}>
-              <BiUser size="20" /> Username
-            </label>
-          </div>
-          <div className={styles.input}>
-            <input className={styles.inputUser} type="password" required />
-            <label className={styles.labelUser}>
-              <BiLockAlt size="20" /> Contraseña
-            </label>
-          </div>
-          <button type="submit" className={styles.btnSignIn}>
-            Iniciar Sesión
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.prueba}>
+            <div className={styles.section}>
+                <img src={logo} alt="MercaTodo" />
+                <p className={styles.merca}>
+                    Merca<span className={styles.todo}>Todo</span>
+                </p>
+                <p className={styles.leyenda}>
+                    Bienvenido a su mercado virtual
+                </p>
+            </div>
+            <div>
+                <form className={styles.containerLogin}>
+                    <div className={styles.input}>
+                        <input
+                            className={styles.inputUser}
+                            type="text"
+                            required
+                        />
+                        <label className={styles.labelUser}>
+                            <BiUser size="20" /> Username
+                        </label>
+                    </div>
+                    <div className={styles.input}>
+                        <input
+                            className={styles.inputUser}
+                            type="password"
+                            required
+                        />
+                        <label className={styles.labelUser}>
+                            <BiLockAlt size="20" /> Contraseña
+                        </label>
+                    </div>
+                    <button type="submit" className={styles.btnSignIn}>
+                        Iniciar Sesión
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 };
